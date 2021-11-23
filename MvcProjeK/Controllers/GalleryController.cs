9d5828570329d983e5e81a9383fcd1f47a7bb866
BEYZA ERDEM <192803045@ogr.cbu.edu.tr>
2021-11-23 14:23:11 +0300

    added gallery

--- MvcProjeK/Controllers/GalleryController.cs
+++ MvcProjeK/Controllers/GalleryController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,12 @@ namespace MvcProjeK.Controllers
     public class GalleryController : Controller
     {
         // GET: Gallery
+        ImageFileManager ifm = new ImageFileManager( new EFImageFileDal());
+
         public ActionResult Index()
         {
-            return View();
+            var files = ifm.GetList();
+            return View(files);
         }
     }
 }
