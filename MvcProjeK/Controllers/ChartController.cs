6feb11581e4d45ccee58678b68c4622b31fcc16f
BEYZA ERDEM <192803045@ogr.cbu.edu.tr>
2022-01-31 17:09:30 +0300

    added charts

---
+++ MvcProjeK/Controllers/ChartController.cs
@@ -0,0 +1,40 @@
+﻿using MvcProjeK.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeK.Controllers
+{
+    public class ChartController : Controller
+    {
+        // GET: Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CategoryChart()
+        {
+            return Json(BlogList(), JsonRequestBehavior.AllowGet);
+        }
+
+        public List<ChartClass> BlogList()
+        {
+            List<ChartClass> ct = new List<ChartClass>();
+            ct.Add(new ChartClass()
+            {
+                CategoryName = "Yazılım",
+                CategoryCount = 8
+            });
+            ct.Add(new ChartClass()
+            {
+                CategoryName = "Matematik",
+                CategoryCount = 10
+            });
+            return ct;
+
+        }
+    }
+}
